911d0e620df6add7b79b1cecc4a85a38faaa25fb
niko <me@nikolasgoebel.com>
2019-12-16 14:56:26 +0100

    Cleanup and tests

--- src/relational_exploration/core.clj
+++ src/relational_exploration/core.clj
@@ -1,52 +1,14 @@
-;; Relational Operations
-;; =====================
-
-;; As Clojure programmers, transforming collections of maps is
-;; our bread and butter. clojure.set encourages us to treat such
-;; collections as relations. Plenty of benefits await:
-;;
-;;   1. Bring strong keywords front and center, helping clarity
-;;   2. Maintain provenance throughout the transformation
-;;   3. Decomplect computation from relational structure
-;;   4. Avoid join-like operations with implicit ordering dependencies
-
-
 (ns relational-exploration.core
-  (:use [clojure.set])
-  (:require
-   [clojure.pprint :as pprint]
-   [clojure.spec.alpha :as s]))
+  "As Clojure programmers, transforming collections of maps is
+  our bread and butter. clojure.set encourages us to treat such
+  collections as relations. Plenty of benefits await:
 
-
-(def data
-  #{{:throughput 100 :run 0 :config :a}
-    {:throughput 100 :run 1 :config :a}
-    {:throughput 100 :run 2 :config :a}
-    {:throughput 100 :run 0 :config :b}
-    {:throughput 100 :run 1 :config :b}
-    {:throughput 100 :run 2 :config :b}})
-
-(def data-1
-  (rename data {:throughput :tp}))
-
-;; e.g.
-;; (select #(= (:config %) :a) data-1)
-;; (select #(< (:run %) 1) data-1)
-
-
-
-;; we introduce the spec-map, a way to quickly combine specs
-;; into a tuple filter. a simple example: {:x #(= % 10) :y #(< % 100)}
-;; this spec-map matches all tuples where (= x 10) and (< y 100)
-
-;; (defn matches-predmap? [predmap tuple]
-;;   (reduce-kv
-;;    (fn [_ k v]
-;;      (let [v'       (get tuple k)
-;;            matches? (if (fn? v) (apply v [v']) (= v v'))]
-;;        (if matches? true (reduced false))))
-;;    true
-;;    predmap))
+  1. Bring strong keywords front and center, helping clarity
+  2. Maintain provenance throughout the transformation
+  3. Decomplect computation from relational structure
+  4. Avoid join-like operations with implicit ordering dependencies"
+  (:refer-clojure :exclude [derive])
+  (:require [clojure.spec.alpha :as s]))
 
 (defn matches-specmap? [specmap m]
   (reduce-kv
@@ -58,31 +20,21 @@
    specmap))
 
 (defn where
-  "Returns a set of the elements conforming to the given spec-map."
+  "Returns a set of the elements conforming to the given spec-map. A
+  spec-map is a way to quickly combine specs into a tuple filter.
+
+  Example
+  =======
+  {:x #(= % 10) :y #(< % 100)} matches all tuples where (= x 10) and (< y 100)."
   ([specmap]
    (filter (partial matches-specmap? specmap)))
   ([specmap xset]
    (filter (partial matches-specmap? specmap) xset)))
 
-;; e.g.
-;; (where {:config #(= % :a)} data-1)
-;; (where {:run #(< % 1)} data-1)
-;; (where {:config #(= % :a) :run #(< % 1)} data-1)
-;; (where {:config #{:a :b}} data-1)
-;; (where {:run (s/and even? #(> % 0))} data-1)
-;; (into #{}
-;;       (comp
-;;        (where {:config #(= % :b)})
-;;        (where {:run #(< % 1)}))
-;;       data-1)
-
-
-
-;; we want to provide ways to non-destructively derive information
-;; from tuples, using regular, "destructive" functions
-
 (defn derive
-  "Maps a tuple operation over the relation, associng the result as a new attribute."
+  "Maps a tuple operation over the relation, associng the result as a
+  new attribute. This allows us to non-destructively derive
+  information from tuples using regular, 'destructive' functions."
   ([dest-attr f]
    (map (fn [tuple] (assoc tuple dest-attr (apply f [tuple])))))
   ([dest-attr f xrel & args]
@@ -90,43 +42,20 @@
         (map (fn [tuple] (assoc tuple dest-attr (apply f tuple args))))
         (into #{}))))
 
-;; e.g.
-;; (derive :foo (fn [_] (rand-int 100)) data-1)
-;; (derive :foo update data-1 :run inc)
-;; (into #{}
-;;       (comp
-;;        (where {:config #(= % :a)})
-;;        (derive :hash hash))
-;;       data-1)
-
 (defn derive-k
   "Corresponds to SELECT F(key, key2, ...)"
   ([dest-attr f ks]
    (map (fn [tuple]
           (let [param-fn (apply juxt ks)
-       ; @TODO combine juxt with flatten? then a single key-fn may return more than one paramater
+                ;; @TODO combine juxt with flatten? then a single key-fn may return more than one parameter
                 params   (param-fn tuple)]
             (assoc tuple dest-attr (apply f params))))))
   ([dest-attr f ks xrel]
    (into #{} (derive-k dest-attr f ks) xrel)))
 
-;; e.g.
-;; (derive-k :combined-name str [:config :run] data-1)
-;; (derive-k :tp2 #(* % %) [:tp] data-1)
-;; (into #{}
-;;  p     (comp
-;;        (where {:config :a})
-;;        (derive :hash hash)
-;;        (derive-k :tp2 #(* % %) [:tp]))
-;;       data-1)
-
-
-
-;; we must also provide a way to lift data into a relation
-;; the goal of this is to put keywords front and center
-;; use join additionally, whenever a single relation's paths have diverged
-
-(defn relate [& pairs]
+(defn relate
+  "Lift unkeyed data into a relation."
+  [& pairs]
   (assert (even? (count pairs)) "relate requires an even number of arguments")
   (->> pairs
        (partition 2)
--- test/relational_exploration/core_test.clj
+++ test/relational_exploration/core_test.clj
@@ -1,7 +1,106 @@
 (ns relational-exploration.core-test
+  (:refer-clojure :exclude [derive])
   (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as s]
             [relational-exploration.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-where
+  (let [data #{{:throughput 100 :run 0 :config :a}
+               {:throughput 100 :run 1 :config :a}
+               {:throughput 100 :run 2 :config :a}
+               {:throughput 100 :run 0 :config :b}
+               {:throughput 100 :run 1 :config :b}
+               {:throughput 100 :run 2 :config :b}}]
+    (is
+     (= (into #{} (where {:config #{:b}}) data)
+        #{{:throughput 100 :run 0 :config :b}
+          {:throughput 100 :run 1 :config :b}
+          {:throughput 100 :run 2 :config :b}}))
+
+    (is
+     (= (into #{} (where {:run #(< % 1)}) data)
+        #{{:throughput 100 :run 0 :config :a}
+          {:throughput 100 :run 0 :config :b}}))
+
+    (is
+     (= (into #{} (where {:config #{:a}
+                          :run    #(< % 1)}) data)
+        #{{:throughput 100 :run 0 :config :a}}))
+    
+    (is
+     (= (into #{} (where {:config #{:a :b}}) data)
+        data))
+
+    (is
+     (= (into #{} (where {:run (s/and even? pos?)}) data)
+        #{{:throughput 100 :run 2 :config :a}
+          {:throughput 100 :run 2 :config :b}}))
+
+    (is
+     (= (into #{}
+              (comp
+               (where {:config #{:b}})
+               (where {:run zero?}))
+              data)
+        #{{:throughput 100 :run 0 :config :b}}))))
+
+(deftest test-derive
+  (let [data #{{:run 0 :config :a}
+               {:run 1 :config :a}
+               {:run 2 :config :a}
+               {:run 0 :config :b}
+               {:run 1 :config :b}
+               {:run 2 :config :b}}]
+    (is
+     (= (into #{} (derive :foo (fn [_] "xyz")) data)
+        #{{:run 0 :config :a :foo "xyz"}
+          {:run 1 :config :a :foo "xyz"}
+          {:run 2 :config :a :foo "xyz"}
+          {:run 0 :config :b :foo "xyz"}
+          {:run 1 :config :b :foo "xyz"}
+          {:run 2 :config :b :foo "xyz"}}))
+
+    (is
+     (= (into #{}
+              (comp
+               (where {:run zero?})
+               (derive :hash hash))
+              #{{:run 0 :config :a}
+                {:run 1 :config :a}})
+        #{{:run 0 :config :a :hash 2037472533}}))))
+
+(deftest test-derive-k
+  (let [data #{{:tp 100 :run 0 :config :a}
+               {:tp 100 :run 0 :config :b}
+               {:tp 100 :run 1 :config :b}}]
+    (is
+     (= (into #{} (derive-k :name str [:config :run]) data)
+        #{{:tp 100 :run 0 :config :a :name ":a0"}
+          {:tp 100 :run 0 :config :b :name ":b0"}
+          {:tp 100 :run 1 :config :b :name ":b1"}}))
+
+    (is
+     (= (into #{} (derive-k :tp2 #(* % %) [:tp]) data)
+        #{{:tp 100 :run 0 :config :a :tp2 10000}
+          {:tp 100 :run 0 :config :b :tp2 10000}
+          {:tp 100 :run 1 :config :b :tp2 10000}}))
+
+    (is
+     (= (into #{}
+              (comp
+               (where {:config #{:a}})
+               (derive :hash hash)
+               (derive-k :tp2 #(* % %) [:tp]))
+              data)
+        #{{:tp 100 :run 0 :config :a :hash -463536415 :tp2 10000}}))))
+
+(deftest test-relate
+  (is
+   (= (into #{} (relate :k [:a :b "c"]))
+      #{{:k :a} {:k :b} {:k "c"}}))
+
+  (is
+   (= (into #{} (relate :idx (range 3) :y (range 3)))
+      #{{:idx 0 :y 0}
+        {:idx 1 :y 1}
+        {:idx 2 :y 2}})))
